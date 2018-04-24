afc70dc60467b15b566364712c2590a2de64445e
almendranc1994 <almendra.nunezc@pucp.pe>
2018-04-24 05:19:59 -0500

    visa detalle docente version 1.1

--- src/components/PerfilProfesor.js
+++ src/components/PerfilProfesor.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React from 'react';
-import * as ReactBootstrap from 'react-bootstrap';
+
+//import * as ReactBootstrap from 'react-bootstrap';
+
+import {Panel,Image, Col, Grid, Row} from 'react-bootstrap';
 
 
 class PerfilProfesor extends React.Component {
@@ -16,8 +19,14 @@ class PerfilProfesor extends React.Component {
             correo: '',
             seccion: '',
             imagen: null,
-        }
+        };
 
+        /*axios.get('http://localhost:3000/profesores/perfilprofesor?codigo=1234578')
+            .then(res => {
+                return res.json()
+            }).then(response => {
+            this.setState({tipo: response.tipo, nombre: response.nombre, telefono: response.telefono, correo: response.correo, seccion: response.seccion})
+        })*/
     }
 
     /*componentDidMount(){
@@ -44,29 +53,52 @@ class PerfilProfesor extends React.Component {
         var telefono = this.state.telefono;
         var correo = this.state.correo;
         var seccion = this.state.seccion;
+        var detalleDocente = require("react-table").default;
         const imgUrl = require('../resources/images/'+ codigo + '.PNG');
+        return(
+                
+            <Row className="show-grid">
+                <Col md={3}>
 
-        return (
-            <div >
-                <div className='Detalles'>
-                    <h4>Codigo: {codigo}</h4>
-                    <h4>Tipo: {tipo}</h4>
-                    <h4>Docente: {nombre}</h4>
-                    <h4>Telefono: {telefono}</h4>
-                    <h4>Correo: {correo}</h4>
-                    <h4>Seccion: {seccion}</h4>
-                </div>
-                <div>
-                    <ReactBootstrap.Image
-                        className='avatar'
-                        src={imgUrl}
-                        alt={'Avatar for ' + codigo}
-                        responsive
-                        rounded
-                    />
-                </div>
-            </div>
+                </Col>
+                <Col md={6}>
+                    <Panel bsStyle="primary">
+                        <Panel.Heading>
+                            <Panel.Title componentClass="h3">Detalle Docente</Panel.Title>
+                        </Panel.Heading>
+                        <Panel.Body>
+                            <Row className="show-grid">
+                                <Col md={6}>
+                                    <h4>Codigo: {codigo}</h4>
+                                    <h4>Tipo: {tipo}</h4>
+                                    <h4>Docente: {nombre}</h4>
+                                    <h4>Telefono: {telefono}</h4>
+                                    <h4>Correo: {correo}</h4>
+                                    <h4>Seccion: {seccion}</h4>
+                                </Col>
+                                <Col md={6}>
+                                    <Image
+                                        className='avatar'
+                                        src={imgUrl}
+                                        alt={'Avatar for ' + codigo}
+                                        responsive
+                                        rounded
+                                    />
+                                </Col>
+                            </Row>
+                        </Panel.Body>
+                    </Panel>
+                </Col>
+                <Col md={3}>
+
+                </Col>
+
+
+            </Row>
+
+
         );
+
     }
 }
 
