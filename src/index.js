611fba6a95df06da313125416a4eb9a2f24b27bc
Walter Reyes Olórtegui <w.reyeso@pucp.pe>
2018-05-02 07:37:58 -0500

    Pantalla de actividades por profesor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './containers/App';
+import ListaActividades from './components/ListaActividades';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ListaActividades />, document.getElementById('root'));
 registerServiceWorker();
