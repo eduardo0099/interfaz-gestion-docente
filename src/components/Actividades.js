c5f3a9148f789ca67ad57c3b49dc71944b69d464
almendranc1994 <almendra.nunezc@pucp.pe>
2018-05-12 19:42:38 -0500

    Ventana actividades faltan botones modificar y eliminar

--- src/components/Actividades.js
+++ src/components/Actividades.js
@@ -4,29 +4,28 @@ import 'react-table/react-table.css';
 import axios from "axios/index";
 import BootstrapTable from 'react-bootstrap-table-next';
 import RegistroActividad from "./RegistroActividad"
+import ModificarActividad from "./ModificarActividad";
 import {HashRouter ,BrowserRouter,Router,Route,Link} from 'react-router-dom';
 import {Grid,Row,Table,Button, Glyphicon,Col} from 'react-bootstrap';
+import './../styles/ListaActividades.css';
 
 export class Actividades extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            selectedId:-1,
             actividades: [
                 {
-                    titulo:"aaa",
-                    tipo:"congreso",
+                    id:"",
+                    titulo:"",
+                    tipo:"",
                     fecha_inicio:"",
                     fecha_fin:"",
                     estado:"",
                 }
             ],
-            ciclos: [
-                {
-                    id:1,
-                    descripcion:"2018-1"
-                }
-            ],
+            ciclos:[],
             cicloSeleccionado: ""
         }
     }
@@ -36,18 +35,51 @@ export class Actividades extends React.Component {
             .then(response => {
                 this.setState({
                     actividades: response.data.actividades
-                });
+                })
             })
             .catch(error => {
-                console.log(`Error al obtener datos del profesor ${this.props.match.params.codigo}`,error);
-            });
+                console.log(`Error al obtener datos de la actividad ${this.props.match.params.codigo}`,error);
+            })
+    }
+
+    eliminar = () =>{
+        if(this.state.selectedId!=-1){
+            if (window.confirm('Seguro que deseas eliminar esta actividad?')) {
+                // Save it!
+                axios.delete('http://200.16.7.151:8080/docente/actividad/eliminar', {
+                    data:{
+                        id:this.state.selectedId
+                    }
+                })
+                    .then(function (response) {
+                        alert("Actividad eliminada");
+                    })
+                    .catch(function (error) {
+                        alert("Error: No se pudo eliminar la actividad");
+                    })
+            } else {
+                // Do nothing!
+            }
+        }else{
+            alert(`Seleccionar una actividad!`);
+        }
+    }
+
+    modificar = () =>{
+        if(this.state.selectedId==-1){
+            alert(`Seleccionar una actividad!`);
+        }
     }
 
     render () {
         console.log(this.state);
 
-        const columnas = [
+        const columns = [
             {
+               dataField: 'id',
+               text: 'ID',
+               hidden: true
+            }, {
                 text: 'Nombre de la Actividad',
                 dataField: 'titulo'
             }, {
@@ -64,22 +96,53 @@ export class Actividades extends React.Component {
                 dataField: 'estado',
             }
         ];
+        const selectRow = {
+            mode: 'radio',
+            clickToSelect: true
+        };
+
+        const rowEvents = {
+            onClick: (e, row) => {
+                this.setState({
+                    selectedId: row.id
+                });
+                //alert(`clicked on row with index: ${this.state.selectedId}`);
+            }
+        };
+
+        console.log(this.props)
+
+        function myFunction(x) {
+            console.log("Row index is: " + x.rowIndex);
+        }
+
+        let myComponent;
+        if(this.state.selectedId !== -1) {
+            myComponent = <Link to={`${this.props.match.url}/${this.state.selectedId}/ModificarActividad`}>Modificar</Link>
+        } else {
+            myComponent = <label onClick={this.modificar}>Modificar</label>
+        }
 
         return(
             <div>
                 <Route exact path={`${this.props.match.path}`} render={() =>
                     <Grid>
-                        <select ref="selectorTipo">
-                            {this.state.ciclos.map((item,i)=>{
-                                return <option key={i}>{item.descripcion}</option>
-                            })}
-                        </select>
-                        <BootstrapTable keyField='id' data={ this.state.actividades } columns={ columnas }/>
-                        <Link to={`${this.props.match.url}/registroActividad`}>Registrar</Link>
+                        <Col md={12}>
+                            <Col md={12}>
+                                <BootstrapTable keyField='id' data={ this.state.actividades } columns={ columns }
+                                                selectRow={ selectRow } rowEvents={ rowEvents } />
+
+                            </Col>
+                            <Col md={2}>
+                                <Link to={`${this.props.match.url}/RegistroActividad`}>Registrar</Link>
+                                <Button onClick={this.eliminar}>Eliminar</Button>
+                                {myComponent}
+                            </Col>
+                        </Col>
                     </Grid>
                 }/>
-                <Route path={`${this.props.match.path}/registroActividad`} render={()=> <RegistroActividad/>}/>
-
+                <Route path={`${this.props.match.path}/RegistroActividad`} component={RegistroActividad}/>
+                <Route path={`${this.props.match.path}/:idActividad/ModificarActividad`} component={ModificarActividad}/>
             </div>
         )
     }
---
+++ src/components/ModificarActividad.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ModificarActividad extends React.Component{
+
+    constructor(props){
+        super(props)
+    }
+
+}
+
+export default ModificarActividad;
