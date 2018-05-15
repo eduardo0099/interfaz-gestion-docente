8110547b11d3799d35c65fc8afbd6adc3e9898c2
Johana Gamboa <johana27094@hotmail.com>
2018-05-15 18:06:20 -0500

    Modificaciones no relevantes

--- src/components/DescargaHoras.js
+++ src/components/DescargaHoras.js
@@ -30,7 +30,7 @@ class DescargaHoras extends React.Component{
             return axios.get('http://200.16.7.151:8080/docente/docente/horaDescDocente', {
                 params: {
                     codigo: this.props.match.params.codigo,
-                    ciclo: "2018-1"
+                    ciclo: cicloSeleccionado
                 }
             });
         }))
@@ -47,45 +47,63 @@ class DescargaHoras extends React.Component{
     }
 
     render(){
+        console.log(this.props);
         const columnas=[
             {text:'Nombre del curso',dataField:'nombre'},
             {text:'Codigo',dataField:'codigo'},
             {text:'Horas Descarga',dataField:'hDescargaTotal'}
         ];
-        return <div>
-            <Grid>
-                <Row className="back-bar">
-                    <Col md={12}>
-                        <Button onClick={this.props.history.goBack}><Glyphicon glyph="arrow-left"></Glyphicon></Button>
-                        <span
-                            className="professor-name"> Regresar a perfil docente </span>
-                    </Col>
-                </Row>
-                <Row><h1>Descarga de Horas</h1></Row>
-                <Row>
-                    <Col md={12}>
-                        <p>Ciclo :
-                            <select ref="selectorCiclos" onChange={this.cambioCiclo}>
-                                {this.state.listaCiclos.map((item,i)=>{
-                                    return <option key={i} value={item.descripcion}>{item.descripcion}</option>
-                                })}
-                            </select>
-                        </p>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={12}>
-                        <BootstrapTable keyField='id' data={ this.state.descargas } columns={ columnas }/>
-                    </Col>
-                </Row>
 
-            </Grid>
-            <Route path={`${this.props.match.path}/Detalle_DescargaHoras`} render={ () =>
-                <Detalle_DescargaHoras semana = {[this.state.descargas.semana]}
-            />}/>
-        </div>;
+        const selectRow ={
+            mode: 'checkbox',
+            clickToSelect: true,
+            hideSelectColumn: true,
+            bgColor: '#00BFFF'
+        };
+
+        return(
+            <div>
+                <Grid>
+                    <Row className="back-bar">
+                        <Col md={12}>
+                            <Button onClick={this.props.history.goBack}><Glyphicon glyph="arrow-left"></Glyphicon></Button>
+                            <span
+                                className="professor-name"> Regresar a perfil docente </span>
+                        </Col>
+                    </Row>
+                    <Row><h1>Descarga de Horas</h1></Row>
+                    <Row>
+                        <Col md={12}>
+                            <p>Ciclo :
+                                <select ref="selectorCiclos" onChange={this.state.cicloSelect}>
+                                    {this.state.listaCiclos.map((item,i)=>{
+                                        return <option key={i} value={item.descripcion}>{item.descripcion}</option>
+                                    })}
+                                </select>
+                            </p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={12}>
+                            <BootstrapTable
+                                keyField='id'
+                                data={ this.state.descargas }
+                                columns={ columnas }
+                                selectRow={selectRow}/>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={10}>
+                        </Col>
+                        <Col md={2}>
+                            <Link to={`${this.props.match.url}/Detalle_DescargaHoras`}>Detalle</Link>
+                        </Col>
+                    </Row>
+                </Grid>
+                <Route path={`${this.props.match.path}/Detalle_DescargaHoras`} component={Detalle_DescargaHoras}/>
+            </div>
+        )
     }
-
 }
 
 export default DescargaHoras;
--- src/components/Detalle_DescargaHoras.js
+++ src/components/Detalle_DescargaHoras.js
@@ -1,46 +1,45 @@
 import React, {Component} from 'react';
 import {Grid, Row, Table, Button, Glyphicon, Col, SplitButton, MenuItem} from 'react-bootstrap';
 import axios from "axios/index";
-
+import BootstrapTable from 'react-bootstrap-table-next';
 
 class Detalle_DescargaHoras extends React.Component{
 
     constructor(props){
         super(props);
         this.state = {
-            semana: []
+            semanas: ['se']
         }
     }
 
-
-
+    componentDidMount(){
+        return axios.get('http://200.16.7.151:8080/docente/docente/horaDescDocente/Detalle_DescargaHoras')
+    }
 
     render(){
+        const columnas=[
+            {text:'Semana',dataField:'semana'},
+            {text:'Horas Descarga',dataField:'horas_Descarga'},
+            {text:'Motivo',dataField:'motivo'}
+        ];
         return <div>
             <Grid>
                 <Row className="back-bar">
                     <Col md={12}>
                         <Button onClick={this.props.history.goBack}><Glyphicon glyph="arrow-left"></Glyphicon></Button>
                         <span
-                            className="professor-name"> Regresar a perfil docente </span>
+                            className="professor-name"> Regresar a Descarga de Horas </span>
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={12}>
-                        <Table striped bordered condensed hover>
-                            <thead>
-                            <tr>
-                                <th>Semana</th>
-                                <th>Horas Descarga</th>
-                                <th>Motivo</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </Table>
-                    </Col>
+                    <h1>Detalle de descarga de horas</h1>
                 </Row>
-
+                    <Col md={12}>
+                        <BootstrapTable
+                            keyField='id'
+                            data={ this.state.semanas }
+                            columns={ columnas }/>
+                    </Col>
             </Grid>
         </div>;
     }
