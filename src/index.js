33682a28917f05a0caf89a2af0c4072bee7e2bb0
Walter Reyes Olórtegui <w.reyeso@pucp.pe>
2018-05-16 17:14:38 -0500

    Actividades

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import App from './containers/App';
 import ListaActividades from './components/ListaActividades';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<ListaActividades />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
