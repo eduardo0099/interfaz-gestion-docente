c06aaea21acae6e5a54db29445aae82db04fcdce
almendranc1994 <almendra.nunezc@pucp.pe>
2018-05-22 11:40:45 -0500

    nueva convocatoria

--- src/components/Actividades.js
+++ src/components/Actividades.js
@@ -2,17 +2,12 @@ import React from 'react';
 import 'react-table/react-table.css';
 import axios from "axios/index";
 import '../styles/Actividades.css';
-<<<<<<< HEAD
-import 'react-datepicker/dist/react-datepicker.css';
-import { BootstrapTable, TableHeaderColumn, InsertButton } from 'react-bootstrap-table';
-=======
 import {Button, Col, Grid} from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 import {Route} from 'react-router-dom';
 import SkyLight from 'react-skylight';
 import DatePicker from 'react-date-picker'
 import BaseContainer from "./BaseContainer";
->>>>>>> develop
 
 export class Actividades extends React.Component {
     constructor(props) {
@@ -31,8 +26,9 @@ export class Actividades extends React.Component {
             ],
             ciclos: [],
             cicloSeleccionado: "",
+            dateInit: new Date(),
+            dateFin: new Date(),
         }
-
     }
 
     componentDidMount() {
@@ -47,45 +43,6 @@ export class Actividades extends React.Component {
             })
     }
 
-<<<<<<< HEAD
-    handleInsertButtonClick = (onClick) => {
-        // Custom your onClick event here,
-        // it's not necessary to implement this function if you have no any process before onClick
-        console.log('This is my custom function for InserButton click event');
-        onClick();
-    }
-
-    createCustomInsertButton = (openModal) => {
-        return (
-            <InsertButton
-                btnText='Nueva Actvidad'
-                btnContextual='btn-warning'
-                className='my-custom-class'
-                btnGlyphicon='glyphicon-edit'
-                onClick={ openModal }/>
-        );
-    }
-
-    render(){
-        function onAfterInsertRow(row) {
-            let newRowStr = '';
-            for (const prop in row) {
-                newRowStr += prop + ': ' + row[prop] + ' \n';
-            }
-            axios.post('http://200.16.7.151:8080/docente/actividad/registrar',{
-                idProfesor: "20112728",
-                ciclo: "2018-1",
-                lugar: "PUCP"
-            })
-            alert('La nueva actividad es:\n ' + newRowStr);
-        }
-
-        function onAfterDeleteRow(rowKeys) {
-            for (let i = 0; i < rowKeys.length; i++){
-                axios.delete('http://200.16.7.151:8080/docente/actividad/eliminar', {
-                    data:{
-                        id: rowKeys[i].id
-=======
     guardar = () => {
         var titulo = document.getElementsByName("titulo")[0].value;
         var fechaIni = document.getElementsByName("fechaIni")[0].value;
@@ -99,14 +56,17 @@ export class Actividades extends React.Component {
                 axios.delete('http://200.16.7.151:8080/docente/actividad/eliminar', {
                     data: {
                         id: this.state.selectedId
->>>>>>> develop
                     }
                 })
+                    .then(function (response) {
+                        alert("Actividad eliminada");
+                    })
+                    .catch(function (error) {
+                        alert("Error: No se pudo eliminar la actividad");
+                    })
+            } else {
+                // Do nothing!
             }
-<<<<<<< HEAD
-            alert('Se elimaron las siguientes actividades:' + rowKeys);
-        }
-=======
         } else {
             alert(`Seleccionar una actividad!`);
         }
@@ -115,39 +75,28 @@ export class Actividades extends React.Component {
 
     render() {
         console.log(this.state);
->>>>>>> develop
 
-        function afterSearch(searchText, result) {
-            console.log('La actividad buscada es: ' + searchText);
-            console.log('Resultado:');
-            for (let i = 0; i < result.length; i++) {
-                console.log('Actividad: ' + result[i].id + ', ' + result[i].titulo + ', ' + result[i].tipo + ', ' + result[i].fecha_inicio + ',' + result[i].fecha_fin + ', ' + result[i].estado );
+        const columns = [
+            {
+                dataField: 'id',
+                text: 'ID',
+                hidden: true
+            }, {
+                text: 'Nombre de la Actividad',
+                dataField: 'titulo'
+            }, {
+                text: 'Tipo',
+                dataField: 'tipo'
+            }, {
+                text: 'Fecha Inicio',
+                dataField: 'fecha_inicio'
+            }, {
+                text: 'Fecha Fin',
+                dataField: 'fecha_fin',
+            }, {
+                text: 'Estado',
+                dataField: 'estado',
             }
-<<<<<<< HEAD
-        }
-
-        const options = {
-            insertBtn: this.createCustomInsertButton,
-            afterInsertRow: onAfterInsertRow,
-            afterDeleteRow: onAfterDeleteRow,
-            afterSearch: afterSearch
-        };
-
-        const selectRowProp = {
-            mode: 'checkbox',
-            bgColor: 'pink'
-        };
-
-        return(
-            <BootstrapTable data = {this.state.actividades} striped={true} hover={true} search={ true } insertRow deleteRow={ true } selectRow={ selectRowProp } options={options}>
-                <TableHeaderColumn dataField='id' isKey  hidden hiddenOnInsert searchable={ false }>Id Actividad</TableHeaderColumn>
-                <TableHeaderColumn dataField='titulo'>Nombre Actividad</TableHeaderColumn>
-                <TableHeaderColumn dataField='tipo' searchable={ false }>Tipo</TableHeaderColumn>
-                <TableHeaderColumn dataField='fecha_inicio' searchable={ false }>Fecha Inicio</TableHeaderColumn>
-                <TableHeaderColumn dataField='fecha_fin' searchable={ false }>Fecha Fin</TableHeaderColumn>
-                <TableHeaderColumn dataField='estado' searchable={ false }>Estado</TableHeaderColumn>
-            </BootstrapTable>
-=======
         ];
         const selectRow = {
             mode: 'radio',
@@ -235,7 +184,6 @@ export class Actividades extends React.Component {
                     </div>
                 </BaseContainer>
             </div>
->>>>>>> develop
         );
     }
 }
--- src/components/convocatorias/ConvocatoriasNuevo.js
+++ src/components/convocatorias/ConvocatoriasNuevo.js
@@ -8,15 +8,25 @@ import SimpleReactValidator from "simple-react-validator";
 import moment from "moment";
 import BootstrapTable from 'react-bootstrap-table-next';
 import BaseContainer from "../BaseContainer";
+import {Checkbox, CheckboxGroup} from 'react-checkbox-group';
+
+const gradosAcademicos = [
+    'Título Profesional',
+    'Maestría',
+    'Doctorado',
+    'Diplomatura'
+]
 
 class ConvocatoriaNuevo extends Component {
-
 
     constructor(props) {
         super(props);
         this.validator = new SimpleReactValidator();
         this.state = {
-            gradosAcademicos: [],
+            gradosAcademicos: ['Título Profesional', 'Maestría', 'Doctorado', 'Diplomatura'],
+            docencia: ['Cargos a su curso', 'Asesoria de Tesis', 'Premios a la docencia'],
+            experienciaProfesional: ['Solicitar Experiencia Profesional'],
+            investigacion: ['solInvestigacion'],
             titulo: '',
             autor: [],
             resumen: '',
@@ -30,14 +40,89 @@ class ConvocatoriaNuevo extends Component {
             selectedQuitar: [],
             autoresModal: []
         };
+        this.handleTitulo = this.handleTitulo.bind(this);
+        this.handleAutor = this.handleAutor.bind(this);
+        this.handleResumen = this.handleResumen.bind(this);
+        this.handleFIni = this.handleFIni.bind(this);
+        this.handleFFin = this.handleFFin.bind(this);
     }
 
+    handleTitulo(event) {
+        this.setState({titulo: event.target.value});
+    }
+
+    handleAutor(event) {
+        this.setState({autor: event.target.value});
+        console.log(this.state.autor);
+    }
+
+    handleResumen(event) {
+        this.setState({resumen: event.target.value});
+    }
+
+    handleFIni(date) {
+        this.setState({fecha_inicio: date});
+    }
+
+    handleFFin(date) {
+        this.setState({fecha_fin: date});
+    }
+
+    armarFecha(date){
+        var cadena="";
+        cadena=cadena+date.getFullYear();
+
+        if (date.getMonth()<9){
+            cadena=cadena+0+(date.getMonth()+1);
+        }else{
+            cadena=cadena+(date.getMonth()+1);
+        }
+
+        if (date.getDate()<10){
+            cadena=cadena+0+date.getDate();
+        }else{
+            cadena=cadena+date.getDate();
+        }
+        console.log(cadena);
+        return cadena;
+    }
+
+    validDates(fFin,fIni){
+        var isafter = moment(fFin._d).isAfter(fIni._d);
+        console.log('fechas validas?:',isafter)
+        return isafter;
+    }
 
     offlineData() {
         this.set({gradosAcademicos: [{id: 1, nombre: 'Pregrado'}, {id: 2, nombre: 'Maestría'}, {id: 3, nombre: 'Doctorado'}]})
     }
 
+    gradosAcademicosChanged = (newGradoAcademico) => {
+        this.setState({
+            gradosAcademicos: newGradoAcademico
+        });
+    }
+
+    docenciaChanged = (newDocencia) => {
+        this.setState({
+            docencia: newDocencia
+        });
+    }
+
+    experienciaProfesionalChanged = (newexperProfe) => {
+        this.setState({
+            experienciaProfesional: newexperProfe
+        });
+    }
+
+    investigacionProfesionalChanged = (newinvestigacion) => {
+        this.setState({
+            investigacion: newinvestigacion
+        });
+    }
+
     render() {
+
         return (
             <BaseContainer>
                 <div className="panel col-lg-offset-2 col-lg-8 col-md-12 col-sm-12">
@@ -51,12 +136,82 @@ class ConvocatoriaNuevo extends Component {
                         <h4> Datos generales </h4>
                         <hr/>
                         <div className="form-group">
-                            <label> Titulo </label>
+                            <label> Curso </label>
                             <input className="form-control"/>
                         </div>
                         <div className="form-group">
-                            <label> Curso </label>
+                            <label> Descripción </label>
                             <input className="form-control"/>
+                        </div>
+                        <div className="form-group">
+                            Fecha Inicio:
+                            <DatePicker
+                                dateFormat="DD/MM/YYYY"
+                                selected={this.state.fecha_inicio}
+                                onChange={this.handleFIni}
+                            />
+                            {this.validator.message('fechaIni', this.state.fecha_inicio, 'required', false, {required: 'Este campo es obligatorio'})}
+                        </div>
+                        <div className="form-group">
+                            Fecha Fin:
+                            <DatePicker
+                                dateFormat="DD/MM/YYYY"
+                                selected={this.state.fecha_fin}
+                                onChange={this.handleFFin}
+                            />
+                            {this.validator.message('fechaFin', this.state.fecha_fin, 'required', false, {required: 'Este campo es obligatorio'})}
+                        </div>
+                    </div>
+                    <div className="panel-body">
+                        <h4> Documentos</h4>
+                        <hr/>
+                        <div className="form-group">
+                            <label> Grados Academicos </label>
+                            <CheckboxGroup
+                                checkboxDepth={2} // This is needed to optimize the checkbox group
+                                name="gradosAcademicos"
+                                value={this.state.gradosAcademicos}
+                                onChange={this.gradosAcademicosChanged}>
+                                <label><Checkbox value="tituloProfesional"/> Título Profesional</label>
+                                <hr/>
+                                <label><Checkbox value="mestria"/> Maestría</label>
+                                <hr/>
+                                <label><Checkbox value="doctorado"/> Doctorado</label>
+                            </CheckboxGroup>
+                        </div>
+                        <div className="form-group">
+                            <label> Docencia </label>
+                            <CheckboxGroup
+                                checkboxDepth={2} // This is needed to optimize the checkbox group
+                                name="docencia"
+                                value={this.state.docencia}
+                                onChange={this.docenciaChanged}>
+                                <label><Checkbox value="cargosCurso"/> Cargos a su curso</label>
+                                <hr/>
+                                <label><Checkbox value="asesoriaTesis"/> Asesoria de Tesis</label>
+                                <hr/>
+                                <label><Checkbox value="premiosDocencia"/> Premios a la Docencia</label>
+                            </CheckboxGroup>
+                        </div>
+                        <div className="form-group">
+                            <label> Experiencia Profesional </label>
+                            <CheckboxGroup
+                                checkboxDepth={2} // This is needed to optimize the checkbox group
+                                name="experienciaProfesional"
+                                value={this.state.experienciaProfesional}
+                                onChange={this.experienciaProfesionalChanged}>
+                                <label><Checkbox value="solExperienciaProfesional"/> Solicitar Experiencia Profesional</label>
+                            </CheckboxGroup>
+                        </div>
+                        <div className="form-group">
+                            <label> Investigacion </label>
+                            <CheckboxGroup
+                                checkboxDepth={2} // This is needed to optimize the checkbox group
+                                name="investigacion"
+                                value={this.state.investigacion}
+                                onChange={this.investigacionProfesionalChanged}>
+                                <label><Checkbox value="solInvestigacion"/> Solicitar Investigacion</label>
+                            </CheckboxGroup>
                         </div>
                     </div>
                     <div className="panel-footer text-right">
