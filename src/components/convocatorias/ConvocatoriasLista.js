b92bd05fefcb0615c7625f6eda7d1912f30334cf
johann.morales <johann.morales@pucp.edu.pe>
2018-06-03 18:16:15 -0500

    files upload movido

--- src/components/convocatorias/ConvocatoriasLista.js
+++ src/components/convocatorias/ConvocatoriasLista.js
@@ -43,21 +43,7 @@ class ConvocatoriasLista extends Component {
         }
     }
 
-    uploadFile(e) {
-        let file = e.target.files[0];
-        let formData = new FormData();
-        formData.append('file', file);
-        API.post('tests/upload',
-            formData,
-            {
-                headers: {'Content-Type': 'multipart/form-data'}
-            }
-        ).then(function () {
-            console.log('Archivo subido');
-        }).catch(function () {
-            console.log('Error al subir el archivo');
-        });
-    }
+
 
     render() {
         return (
@@ -69,11 +55,6 @@ class ConvocatoriasLista extends Component {
                                 <Link className="btn btn-sm btn-primary pull-right m-t-md"
                                       to={"/convocatorias/nuevo"}> Nueva Convocatoria</Link>
                                 <h2> Convocatorias </h2>
-                            </div>
-                            <div className="panel-body">
-                                <label>Files
-                                    <input type="file" id="files" ref="files" onChange={this.uploadFile}/>
-                                </label>
                             </div>
                             <div className="panel-body">
                                 <table className="table table-striped">
