a1252ebddea8689d9d85c16ab442f8f1d93bab77
Johann Morales <johann.morales@pucp.edu.pe>
2018-06-06 00:54:48 -0500

    added authorization header

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
-export default axios.create({
+const api  = axios.create({
     baseURL: 'http://localhost:8080'
 });
+
+api.interceptors.request.use((config)=>{
+    const token = localStorage.getItem("token");
+    //config.headers.Authorization = `Bearer ${token}`;
+    return config;
+})
+
+export default api;
