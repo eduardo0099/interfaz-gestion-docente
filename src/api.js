3f9669fa87cc08dac844b7b7e9fa22c67863404b
negar14 <inoku_0099@hotmail.com>
2018-06-06 20:54:22 -0500

    login x2

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api  = axios.create({
-    baseURL: 'http://200.16.7.151:8090'
+    baseURL: 'http://200.16.7.151:8080'
     //baseURL: 'http://localhost:8090'
 });
 
