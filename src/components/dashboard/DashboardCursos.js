6a73cbf457d1b56582e5c53548a9eab676c731c5
sadTreecko <ruben.camarena@pucp.pe>
2018-06-09 17:24:20 -0500

    mejora dashboard cursos

--- src/components/dashboard/DashboardCursos.js
+++ src/components/dashboard/DashboardCursos.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import {Route} from 'react-router-dom';
 import Select from 'react-select';
-import {Panel,  Dropdown, Glyphicon, MenuItem} from 'react-bootstrap';
+import {Panel,  Dropdown, Glyphicon, MenuItem,Modal} from 'react-bootstrap';
 import BaseContainer from "../BaseContainer";
 import ConvocatoriaNuevo from "../convocatorias/ConvocatoriasNuevo";
 import AyudaEconomicaNuevo from "../AyudaEconomica/AyudaEconomicaNuevo";
@@ -10,30 +10,6 @@ import API from "../../api";
 import AyudaEconomicaDetalle from "../AyudaEconomica/AyudaEconomicaDetalle";
 import AyudaEconomicaAprobar from "../AyudaEconomica/AyudaEconomicaAprobar";
 
-function onlyUnique(array) {
-    var BreakException = {};
-    let aux=[];
-    array.forEach(entry => {
-        let repetido=0;
-        if(aux.length==0){
-            aux=[...aux,entry];
-        }
-
-        try{
-            aux.forEach(entry2=>{
-                if(JSON.stringify(entry)==JSON.stringify(entry2)){
-                    repetido=1;
-                    throw BreakException;
-                }
-            });
-            aux=[...aux,entry];
-        } catch (e) {
-            if (e !== BreakException) throw e;
-        }
-
-    });
-    return aux;
-}
 
 class DashboardCursos extends React.Component {
 
@@ -41,25 +17,26 @@ class DashboardCursos extends React.Component {
         super(props);
 
         this.state = {
+            showDetalleCurso:false,
+            selectedNombre:'',
             cursos: [
                 {
                     id: 20,
-                    codigo_solicitud: 'SOL001',
-                    titulo: 'Investigando las Causas de las Investigaciones 1',
-                    motivo: 'Compra de Materiales',
-                    monto_otorgado: 8000000.63,
-                    estado:'Aprobado',
-                    fechaSolicitud: '12/12/12',
-                    profesor: {
-                        codigo_profesor:20112728,
-                        nombres:"Freddy",
-                        apellido_paterno:"Espinoza",
-                        apellido_materno:"Paz",
-                        correo_pucp:"fpaz@pucp.edu.pe",
-                        tipo_profesor:"TC",
-                        seccion:"Informatica"
-                    },
-                }]
+                    codigo: 'INF666',
+                    nombre: 'Chistemas Operativos',
+                    seccion: 'Chinformatica',
+                    creditos: 20,
+                    tipo:'Pregrado',
+                }],
+            horariosSes:[
+                {
+                    id:1,
+                    horario:"0666",
+                    profesor:"Viktor Khlebnikov",
+                    codigo_prof:"18456666",
+                    sesiones:"LUN 3am-6pm v666"
+                }
+            ]
         }
     }
 /*
@@ -83,49 +60,6 @@ class DashboardCursos extends React.Component {
             })
     }
 
-    findAyudas(ciclo) {
-
-        API.get('ayudasEconomicas/ayudasEconomicas/listar', {
-            params: {
-                ciclo: ciclo,
-            }
-        }).then(response => {
-            let listaCBInvestigaciones=[];
-            let listaCBProfesor=[];
-            let listaCBSeccion=[];
-            let listaCBMotivo=[];
-            let listaCBEstado=[];
-            let listaCBCodigo=[];
-
-            response.data.ayudaEconomica.forEach(function (entry) {
-                listaCBInvestigaciones=[...listaCBInvestigaciones,{titulo:entry.titulo}];
-                listaCBProfesor=[...listaCBProfesor,{codigo:entry.profesor.codigo_profesor,nombres:entry.profesor.nombres +" "+ entry.profesor.apellido_paterno +" "+ entry.profesor.apellido_materno}];
-                listaCBSeccion=[...listaCBSeccion,{seccion:entry.profesor.seccion}];
-                listaCBMotivo=[...listaCBMotivo,{motivo:entry.motivo}];
-                listaCBEstado=[...listaCBEstado,{estado:entry.estado}];
-                listaCBCodigo=[...listaCBCodigo,{codigo:entry.codigo_solicitud,id:entry.id}];
-            });
-            console.log('listaCBProfesor0:',listaCBProfesor[0]);
-            console.log('listaCBProfesor1:',listaCBProfesor[1]);
-            console.log('Prueba obj:');
-            if(JSON.stringify(listaCBProfesor[0])===JSON.stringify(listaCBProfesor[1])){
-                console.log('ala mrdd');
-            }
-            this.setState({
-                ayudas: response.data.ayudaEconomica,
-                ayudasMostrar: response.data.ayudaEconomica,
-                listaCBInvestigaciones:onlyUnique(listaCBInvestigaciones),
-                listaCBProfesor:onlyUnique(listaCBProfesor),
-                listaCBSeccion:onlyUnique(listaCBSeccion),
-                listaCBMotivo:onlyUnique(listaCBMotivo),
-                listaCBEstado:onlyUnique(listaCBEstado),
-                listaCBCodigo:onlyUnique(listaCBCodigo)
-            })
-        }).catch(error => {
-            console.log("Error obteniendo la lista de las investigaciones", error);
-        });
-    }
-
     cambioCiclo = (obj) => {
         let ciclo = obj.descripcion;
         this.setState({ cicloSeleccionado: ciclo })
@@ -133,87 +67,99 @@ class DashboardCursos extends React.Component {
         this.reestablecerFiltros();
     };
 
+    handleMostrarCurso(obj, event){
+        this.setState({showDetalleCurso: true,
+        selectedNombre:obj.nombre});
+    }
+
+    handleClose(){
+        this.setState({showDetalleCurso: false});
+    }
+
     render() {
         return (
             <div>
                 <Route exact path={`${this.props.ruta}`} render={() =>
-                    <BaseContainer>
-                        <div className="panel wrapper-md col-lg-offset-1 col-lg-10 col-md-12 col-sm-12">
-                            <div className="panel-heading">
-                                <h2> Ayudas Económicas </h2>
-                            </div>
-                            <div className="panel-body row">
-                                <div className="col-md-12">
-                                    <Panel>
-                                        <table className="table table-striped">
-                                            <thead>
-                                            <tr>
-                                                <th className="col-md-1 text-center"> Código</th>
-                                                <th className="col-md-3"> Investigación</th>
-                                                <th className="col-md-3"> Profesor</th>
-                                                <th className="col-md-2"> Motivo</th>
-                                                <th className="col-md-2 text-center"> Monto</th>
-                                                <th className="col-md-1 text-center"> Estado</th>
-                                                <th></th>
+                    <div className="panel ">
+                        <div className="panel-body row">
+                            <Panel>
+                                <table className="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th className="col-md-2 "> Curso</th>
+                                        <th className="col-md-2"> Sección</th>
+                                        <th className="col-md-2 text-center"> Créditos</th>
+                                        <th className="col-md-2 text-center"> Tipo</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.cursos.map(curso => {
+                                        return (
+                                            <tr onClick={this.handleMostrarCurso.bind(this,curso)}>
+                                                <td className="v-middle">
+                                                    <span className="block text-primary"> { curso.nombre } </span>
+                                                    <small className="block text-muted"> { curso.codigo } </small>
+                                                </td>
+                                                <td className="v-middle ">
+                                                    <span> {curso.seccion}</span>
+                                                </td>
+                                                <td className="v-middle text-center">
+                                                    <span className="badge badge-blue"> { curso.creditos } </span>
+                                                </td>
+                                                <td className="v-middle text-center">
+                                                    <span> {curso.tipo}</span>
+                                                </td>
                                             </tr>
-                                            </thead>
-                                            <tbody>
-                                            {this.state.cursos.map(ayuda => {
-                                                return (
-                                                    <tr>
-                                                        <td className="v-middle text-center">
-                                                            <b className="text-primary text-md"> {ayuda.codigo_solicitud} </b>
-                                                        </td>
-                                                        <td className="v-middle">
-                                                            <span> {ayuda.titulo}</span>
-                                                        </td>
-                                                        <td className="v-middle">
-                                                            <span className="block"> {ayuda.profesor.nombres} {ayuda.profesor.apellido_paterno}</span>
-                                                            <small
-                                                                className="block text-muted"> {ayuda.profesor.tipo_profesor}</small>
-                                                            <small
-                                                                className="block text-muted"> {ayuda.profesor.seccion}</small>
-                                                        </td>
-                                                        <td className="v-middle">
-                                                            <span> {ayuda.motivo}</span>
-                                                        </td>
-                                                        <td className="v-middle text-center">
-                                                            <span className="text-md"> S/ {ayuda.monto_otorgado} </span>
-                                                        </td>
-                                                        <td className="v-middle text-center">
-                                                            <span className={"label label-" + ({
-                                                                'Aprobado': 'success',
-                                                                'Pendiente': 'warning',
-                                                                'Rechazado': 'danger'
-                                                            }[ayuda.estado])}> {ayuda.estado} </span>
-                                                        </td>
-                                                        <td className="v-middle">
-                                                            <Dropdown className="dropdown-options" pullRight>
-                                                                <Dropdown.Toggle className="dropdown-options" noCaret="true">
-                                                                    <Glyphicon glyph="option-vertical"/>
-                                                                </Dropdown.Toggle>
-                                                                <Dropdown.Menu>
-                                                                    <MenuItem href={'/ayudaeconomica/id/' + ayuda.id}>Ver
-                                                                        Detalle</MenuItem>
-                                                                    <MenuItem href={'/ayudaeconomica/' + ayuda.id + '/Detalle'}>Ver
-                                                                        Detalle JD</MenuItem>
-                                                                </Dropdown.Menu>
-                                                            </Dropdown>
-                                                        </td>
-                                                    </tr>
-                                                )
-                                            })}
-                                            </tbody>
-                                        </table>
-                                    </Panel>
-                                </div>
+                                        )
+                                    })}
+                                    </tbody>
+                                </table>
+                            </Panel>
+                        </div>
+                    </div>
+                }/>
+                <Modal show={this.state.showDetalleCurso} onHide={this.handleClose.bind(this)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{this.state.selectedNombre}: Horarios</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="form-group">
+                            <table className="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th className="col-md-1 "> Horario</th>
+                                    <th className="col-md-2"> Profesor</th>
+                                    <th className="col-md-4 "> Sesiones</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {this.state.horariosSes.map(horarioSes => {
+                                    return (
+                                        <tr>
+                                            <td className="v-middle">
+                                                <span className="block text-primary text-md"> { horarioSes.horario } </span>
+                                            </td>
+                                            <td className="v-middle ">
+                                                <span className="block"> {horarioSes.profesor}</span>
+                                                <small className="block text-muted"> { horarioSes.codigo_prof } </small>
+                                            </td>
+                                            <td className="v-middle">
+                                                <span> { horarioSes.sesiones } </span>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                                </tbody>
+                            </table>
+
+                            <div className=" text-right">
+                                <button className="btn btn-sm  ">Descargar Programa Analítico</button>
                             </div>
                         </div>
-                    </BaseContainer>
-                }/>
-                <Route path={`${this.props.ruta}/:idAyudaEconomica/Detalle`} component={AyudaEconomicaAprobar}/>
-                <Route path={`${this.props.ruta}/id/:idAyudaEconomica`} component={AyudaEconomicaDetalle}/>
+                    </Modal.Body>
+                </Modal>
             </div>
+
         );
     }
 }
