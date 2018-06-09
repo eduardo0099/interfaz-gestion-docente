489ad5a482e393cd0c7ca8e1690f0def9bbde5d3
johann.morales <johann.morales@pucp.edu.pe>
2018-06-09 16:30:12 -0500

    dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -4,17 +4,27 @@ import axios from "axios/index";
 import BaseContainer from "../BaseContainer";
 import API from '../../api'
 import DashboardTabs from './DashboardTabs'
+import Select from 'react-select';
+
 
 class Dashboard extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            secciones: [{id: 1, nombre: 'Seccion 1'}, {id: 2, nombre: 'Seccion 2'}, {id: 1, nombre: 'Seccion 3'}],
+            seccion: {}
+        }
     }
 
     componentDidMount() {
 
     }
+
+
+    cambioSeccion = (obj) => {
+        this.setState({seccion: obj});
+    };
 
     render() {
         return (
@@ -24,14 +34,25 @@ class Dashboard extends Component {
                         <div className="panel col-lg-offset-2 col-lg-8 col-md-12 col-sm-12">
                             <div className="panel-heading m-b-n">
                                 <header className="page-header m-b-n">
-                                    <a className="btn btn-default pull-right"
-                                       onClick={this.props.history.goBack}> Volver </a>
                                     <p className="h2"> Dashboard </p>
                                 </header>
                             </div>
                             <div className="panel-body m-t-n">
-                                <div className="m-t-lg">
-                                    <DashboardTabs/>
+                                <div className="form-group row">
+                                    <div className="col-md-3">
+                                        <label> Seccion </label>
+                                        <Select
+                                            value={this.state.seccion}
+                                            onChange={this.cambioSeccion}
+                                            valueKey={"id"}
+                                            labelKey={"nombre"}
+                                            options={this.state.secciones}
+                                            clearable={false}
+                                        />
+                                    </div>
+                                </div>
+                                <div className="form-group">
+                                    <DashboardTabs seccion={this.state.seccion}/>
                                 </div>
                             </div>
                         </div>
